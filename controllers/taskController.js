fa2ca2dd36a48fc669d314685dbeb3058217cbe5
far_faqiri <farida.faqiri2@gmail.com>
2024-11-24 00:02:11 +0430

    solved the errors

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -1,4 +1,4 @@
-const Task = require("../models/Task");
+const Task = require('../models/Task');
 
 // Get all tasks
 exports.getTasks = async (req, res) => {
--- server.js
+++ server.js
@@ -1,18 +1,18 @@
 const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 const taskRoutes = require("./route/taskRoutes");
-const cors = require('cors');
-const dotenv = require("dotenv");
 
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
 
 connectDB();
-app.use(cors()); // Enable CORS
+app.use(cors());
 app.use(express.json());
 app.use("/api/tasks", taskRoutes);
 
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server is running on http://localhost:${port}`);
 });
